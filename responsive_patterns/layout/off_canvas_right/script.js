d0bba5af60e01a6bbe6f525c17159342bb058b0b
Cythilya Tang <cythilya@gmail.com>
2015-04-20 20:07:00 +0800

    off canvas - right

---
+++ responsive_patterns/layout/off_canvas_right/script.js
@@ -0,0 +1,28 @@
+(function($) {
+	$.fn.offCanvasRight = function(opts) {
+		var config = $.extend({}, {
+			opt: ''
+		}, opts);
+		function init(obj) {
+			var dObj = $(obj);
+			var dWrap = dObj.find('.wrap');
+			var dMenu = dObj.find('.menu'); 
+		    var dMenulink = dObj.find('.menuLink');
+
+		    $('body').addClass('on');
+		  
+			dMenulink.click(function(e) {
+				e.preventDefault();
+				dMenulink.toggleClass('active');
+				dWrap.toggleClass('active');
+			});	
+        }
+		this.each(function() {
+			init($(this));
+		});
+		return this;
+	};
+	$(function() {
+		$('.pattern').offCanvasRight();
+	});
+})(jQuery);
