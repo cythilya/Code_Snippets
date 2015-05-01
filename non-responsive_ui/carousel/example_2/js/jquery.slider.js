7f158a4b80fdc0efa5a3aa5626a69ec04133c41c
Cythilya Tang <cythilya@gmail.com>
2015-05-01 14:35:05 +0800

    slider example 2, jslint

--- non-responsive_ui/carousel/example_2/js/jquery.slider.js
+++ non-responsive_ui/carousel/example_2/js/jquery.slider.js
@@ -1,34 +1,35 @@
-(function($) {
-	$.fn.slider = function(opts) {
-		//default configuration
-		var config = $.extend({}, {
-			speed: 1000, //移動速度
+(function ($) {
+    "use strict";
+    $.fn.slider = function (opts) {
+        //default configuration
+        var config = $.extend({}, {
+            speed: 1000, //移動速度
             waiting: 2000, //等待時間
             quickSpeed: 1000, //利用prev/next button切換slide的速度
             start: -1140, //起始位置(px)
             imageWidth: 560, //每次移動的距離(px)
             controlBtn: true, //是否開啟左右控制button
             autoRun: true //是否自動執行
-		}, opts);
-		//main function
-		function init(obj) {
-            var dSlider = $(obj);
-            var dFrame = dSlider.find('.js-sliderList');
-            var dItemList = dSlider.find('.js-sliderItem');
-            var itemArray = [];
-            var dPrevBtn = dSlider.find('.js-btnPrev');
-            var dNextBtn = dSlider.find('.js-btnNext');
-            var stop = false;
-            var reset = false;
+        }, opts);
+        //main function
+        function init(obj) {
+            var dSlider = $(obj),
+                dFrame = dSlider.find('.js-sliderList'),
+                dItemList = dSlider.find('.js-sliderItem'),
+                itemArray = [],
+                dPrevBtn = dSlider.find('.js-btnPrev'),
+                dNextBtn = dSlider.find('.js-btnNext'),
+                stop = false,
+                reset = false;
 
-            function autoRun(){
+            function autoRun() {
                 var dCurrent;
-                setInterval(function(){
-                    if(!stop){
-                        if(reset){
+                setInterval(function () {
+                    if (!stop) {
+                        if (reset) {
                             itemArray = [];
                             dItemList = dSlider.find('.js-sliderItem');
-                            $.each( dItemList, function( key, value ) {
+                            $.each(dItemList, function (key, value) {
                                 $(value).clone().appendTo(dFrame);
                                 itemArray.push(value);
                             });
@@ -38,7 +39,7 @@
                         dCurrent = $(itemArray.shift());
                         dCurrent.clone().appendTo(dFrame);
                         itemArray.push(dCurrent);
-                        dFrame.animate({left: '-=' + config.imageWidth + 'px'}, config.speed, function(){
+                        dFrame.animate({left: '-=' + config.imageWidth + 'px'}, config.speed, function () {
                             dSlider.find('.js-sliderItem').eq(0).remove();
                             dFrame.css('left', config.start + 'px');
                         });
@@ -46,60 +47,55 @@
                 }, config.waiting);
             }
 
-            dSlider.on('mouseover mouseout', function(e){
-                var dThisWrapper = $(this);
-
-                if(e.type == 'mouseover'){
+            dSlider.on('mouseover mouseout', function (e) {
+                if (e.type === 'mouseover') {
                     stop = true;
-                } 
-                else{ //mouseout
+                } else { //mouseout
                     stop = false;
                 }
             });
 
-            dPrevBtn.click(function(e){
+            dPrevBtn.click(function (e) {
                 var dThisBtn = $(this);
 
                 e.preventDefault();
                 stop = true;
                 reset = true;
-                
-                if(!dThisBtn.hasClass('disabled')){
+                if (!dThisBtn.hasClass('disabled')) {
                     dThisBtn.addClass('disabled');
-                    dFrame.animate({left: '+=' + config.imageWidth + 'px'}, config.quickSpeed, function(){
+                    dFrame.animate({left: '+=' + config.imageWidth + 'px'}, config.quickSpeed, function () {
                         var dLastItem = dSlider.find('.js-sliderItem').last();
                         itemArray.push(dLastItem);
                         dLastItem.remove();
-                        dLastItem.prependTo(dFrame);  
-
+                        dLastItem.prependTo(dFrame);
                         dFrame.css('left', config.start + 'px');
                         dThisBtn.removeClass('disabled');
                     });
                 }
             });
 
-            dNextBtn.click(function(e){
-                var dThisBtn = $(this);
+            dNextBtn.click(function (e) {
+                var dThisBtn = $(this),
+                    dCurrent;
 
                 e.preventDefault();
                 stop = true;
-                
-                if(!dThisBtn.hasClass('disabled')){
+                if (!dThisBtn.hasClass('disabled')) {
                     dThisBtn.addClass('disabled');
-                    var dCurrent =  dSlider.find('.js-sliderItem').eq(0);
+                    dCurrent =  dSlider.find('.js-sliderItem').eq(0);
                     dCurrent.clone().appendTo(dFrame);
                     itemArray.push(dCurrent);
-                    dFrame.animate({left: '-=' + config.imageWidth + 'px'}, config.quickSpeed, function(){
+                    dFrame.animate({left: '-=' + config.imageWidth + 'px'}, config.quickSpeed, function () {
                         dSlider.find('.js-sliderItem').eq(0).remove();
                         dFrame.css('left', config.start + 'px');
                         dThisBtn.removeClass('disabled');
-                    });                    
+                    });
                 }
-            });            
+            });
 
             //init
             //double slide list
-            $.each( dItemList, function( key, value ) {
+            $.each(dItemList, function (key, value) {
                 $(value).clone().appendTo(dFrame);
                 itemArray.push(value);
             });
@@ -108,26 +104,26 @@
             dFrame.css({ 'left': config.start + 'px' });
 
             //enable auto run
-            if(config.autoRun){
-                autoRun();    
+            if (config.autoRun) {
+                autoRun();
             }
 
             //enable control button: prev and next
-            if(!config.controlBtn){
+            if (!config.controlBtn) {
                 dPrevBtn.hide();
                 dNextBtn.hide();
             }
         }
-		//initialize every element
-		this.each(function() {
-			init($(this));
-		});
-		return this;
-	};
-	//auto start
+        //initialize every element
+        this.each(function () {
+            init($(this));
+        });
+        return this;
+    };
+    //auto start
     /*
-	$(function() {
-		$('.js-slider').slider();
-	});
+    $(function() {
+    $('.js-slider').slider();
+    });
     */
 })(jQuery);
