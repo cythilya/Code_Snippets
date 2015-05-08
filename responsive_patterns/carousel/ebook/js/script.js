077671799635cc48efe3b9cdca61134b589c1159
Cythilya Tang <cythilya@gmail.com>
2015-05-08 17:32:31 +0800

    slider

--- responsive_patterns/carousel/ebook/js/script.js
+++ responsive_patterns/carousel/ebook/js/script.js
@@ -12,56 +12,12 @@
             var dNavLink = dObj.find('.navLink');
             var startOrder = 1;
 
-            var eBookData = [
-                {
-                    "id": "1",
-                    "title": "It's PARTY RUN",
-                    "image": "css/dummy_768x1024_pink.png"
-                }                                             
-            ];
-            
-            var navHTMLResult = '';
-            var order = 1;
-            $.each(eBookData, function(index, value){
-                var navHTML = '';
-                navHTML += [
-                    '<li class="navItem">',
-                    '    <a class="navLink" href="#" data-order="' + order + '">' + value.title +'</a>',
-                    '</li>'
-                ].join('');
-                order++;
-                navHTMLResult = navHTMLResult + navHTML;
-            })
-
-            /*
-            var frameHTML = '';
-            frameHTML += [
-                '<div class="image-upload-item clearfix">',
-                '    <img src="' + response + '" alt="上傳圖片">',
-                '    <div class="info">',
-                '        <div class="file-name">'+ $('.file-upload').val() +'</div>',
-                '    </div>',
-                '</div>'
-            ].join(''); 
-            */       
-
             dObj.on('click', '.navLink', function(e){
                 e.preventDefault();
                 var nowOrder = $(this).data('order');
                 var length = -(nowOrder - startOrder)*1024;
                 dFrame.animate({top: length + 'px'}, config.speed); 
-
             });
-
-
-            /*
-            $(window).resize(function(){
-                if(window.innerWidth < 768){
-                }
-                else{
-                }               
-            });
-            */
         }
         // initialize every element
         this.each(function() {
