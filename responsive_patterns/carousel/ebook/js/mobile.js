778917b3dcefcdb80de03e3ae9874317992315af
Cythilya Tang <cythilya@gmail.com>
2015-05-18 17:50:38 +0800

    ebook

--- responsive_patterns/carousel/ebook/js/mobile.js
+++ responsive_patterns/carousel/ebook/js/mobile.js
@@ -3,7 +3,8 @@
         // default configuration
         var config = $.extend({}, {
             speed: 1000,
-            start: 0       
+            start: 0,
+            adShowLimit: 50    
         }, opts);
         // main function
         function init(obj) {
@@ -12,41 +13,80 @@
                 dToggle = dObj.find('.toggle'),
                 dCloseBtn = dObj.find('.btnClose'),
                 dNav = dObj.find('.nav'),
-                dNavLink = dObj.find('.navLink'),
+                dNavLink = dObj.find('.navLink')
+                dAd =  dObj.find('.adArea'),
+                selScrollable = '.scrollable',
                 startOrder = 1,
                 imageHeight = dObj.find('.thumbnail').first().height(),
                 windowHeight = document.documentElement.clientHeight,
-                menuLeft = document.getElementById( 'cbp-spmenu-s1' );
+                menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+                adMaxHeight = 0;
 
-                dObj.find('.item').height(windowHeight);
+            dNavLink.click(function(e){
+                var nowOrder = $(this).data('order');
+                var length = -(nowOrder - startOrder)*windowHeight;
+                e.preventDefault();
+                dFrame.animate({top: length + 'px'}, config.speed); 
+            });
 
-                dNavLink.click(function(e){
-                    var nowOrder = $(this).data('order');
-                    var length = -(nowOrder - startOrder)*windowHeight;
-                    e.preventDefault();
-                    console.log(length);
-                    dFrame.animate({top: length + 'px'}, config.speed); 
-                });
+            dToggle.click(function(e){
+                e.preventDefault();
+                classie.toggle( this, 'active' );
+                classie.toggle( menuLeft, 'cbp-spmenu-open' );
+                disableOther( 'showLeft' );
+            });
 
-                dToggle.click(function(e){
-                    e.preventDefault();
-                    classie.toggle( this, 'active' );
-                    classie.toggle( menuLeft, 'cbp-spmenu-open' );
-                    disableOther( 'showLeft' );
-                });
+            dCloseBtn.click(function(e){
+                e.preventDefault();
+                classie.toggle( this, 'active' );
+                classie.toggle( menuLeft, 'cbp-spmenu-open' );
+                disableOther( 'showLeft' );
+            });
 
-                dCloseBtn.click(function(e){
-                    e.preventDefault();
-                    classie.toggle( this, 'active' );
-                    classie.toggle( menuLeft, 'cbp-spmenu-open' );
-                    disableOther( 'showLeft' );
-                });
-
-                function disableOther( button ) {
-                    if( button !== 'showLeft' ) {
-                        classie.toggle( showLeft, 'disabled' );
+            function disableOther( button ) {
+                if( button !== 'showLeft' ) {
+                    classie.toggle( showLeft, 'disabled' );
                 }
             }
+
+            //init
+            dObj.find('.item').height(windowHeight);
+            adMaxHeight = windowHeight-imageHeight;
+            if(adMaxHeight > config.adShowLimit){
+                dAd.css('max-height', adMaxHeight + 'px');
+                dAd.find('.adImg').css('max-height', adMaxHeight-20  + 'px');
+                dObj.find('.adList').slick({
+                    dots: false,
+                    adaptiveHeight: true,
+                    arrows: false,
+                    mobileFirst: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    swipe: false,
+                    autoplay: true,
+                    lazyLoad: 'progressive'          
+                });
+            } else {
+                dAd.hide();
+            }
+
+            //touch event init
+            $(document).on('touchmove',function(e){
+                e.preventDefault();
+            });
+
+            $('body').on('touchstart', selScrollable, function(e) {
+                if (e.currentTarget.scrollTop === 0) {
+                    e.currentTarget.scrollTop = 1;
+                } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+                    e.currentTarget.scrollTop -= 1;
+                }
+            });
+
+            $('body').on('touchmove', selScrollable, function(e) {
+                e.stopPropagation();
+            });
         }
         // initialize every element
         this.each(function() {
