3aa10660583c49bc6647e2dee1b8c235c8d0878d
Cythilya Tang <cythilya@gmail.com>
2015-07-06 15:08:42 +0800

    design pattern - strategy

--- functional_snippet/design_pattern_strategy/js/script.js
+++ functional_snippet/design_pattern_strategy/js/script.js
@@ -32,45 +32,87 @@ var validator = {
 		}
 		return this.hasErrors();
 	},
-	// helper
+	//helper
 	hasErrors:function () {
 		return this.messages.length !== 0;
 	}
 };
-// checks for non-empty values
+
+//checks for non-empty values
 validator.types.isNonEmpty = {
 	validate:function (value) {
 		return value !== "";
 	},
-	instructions:"the value cannot be empty"
+	instructions: "the value cannot be empty"
 };
-// checks if a value is a number
+
+//checks if a value is a number
 validator.types.isNumber = {
 	validate:function (value) {
 		return !isNaN(value);
 	},
-	instructions:"the value can only be a valid number, e.g. 1, 3.14 or 2010"
+	instructions: "the value can only be a valid number, e.g. 1, 3.14 or 2010"
 };
-// checks if the value contains only letters and numbers
+
+//checks if the value contains only letters and numbers
 validator.types.isAlphaNum = {
 	validate:function (value) {
 		return !/[^a-z0-9]/i.test(value);
 	},
-	instructions:"the value can only contain characters and numbers, no special symbols"
+	instructions: "the value can only contain characters and numbers, no special symbols"
 };
 
+validator.types.isEmail = {
+	validate: function(value){
+		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    	return re.test(value);		
+	},
+	instructions: 'use valid email format, e.g. @'
+};
+
+validator.types.minSize = {
+	validate: function(value){
+    	return value.length >= 3;		
+	},
+	instructions: 'min size is 3 characters'
+};
+
+validator.types.maxSize = {
+	validate: function(value){
+		return value.length <= 10;
+	},
+	instructions: 'max size is 10 characters'
+};
+
+
+/***
+validate types:
+	- isNonEmpty
+	- isNumber
+	- isAlphaNum
+	- isEmail
+	- minSize
+	- maxSize
+***/
 //test
 var data = {
-	first_name: "Super",
-	last_name: "Man",
-	age: "unknown",
-	username: "o_O"
+	first_name: 'Super',
+	last_name: 'Man',
+	age: 'unknown',
+	username: 'o_O',
+	email: 'example@gmail.com',
+	confirm_email: 'invalid_email_sample',
+	password: '12'
 };
 
 validator.config = {
 	first_name: 'isNonEmpty',
+	last_name: 'maxSize',
 	age: 'isNumber',
-	username: 'isAlphaNum'
+	username: 'isAlphaNum',
+	email: 'isEmail',
+	confirm_email: 'isEmail',
+	password: 'minSize'	
 };
 
 validator.validate(data);
