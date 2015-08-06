72b3edc6cbb27e01e4012b470acee049a0a11e45
Cythilya Tang <cythilya@gmail.com>
2015-08-06 12:24:28 +0800

    tab vertical

---
+++ ui/tab_vertical/js/script.js
@@ -0,0 +1,38 @@
+﻿(function($) {
+	$.fn.tabsVertical = function(opts) {
+		// default configuration
+		var config = $.extend({}, { 
+			activeTabNumber: 0 
+		}, opts);
+		// main function
+		function init(obj) {
+            var dTab = obj;
+            var dTabNav = dTab.find('.js-tab-nav');
+            var dTabPanel = dTab.find('.js-tab-panel');
+            
+            dTab.on('click', '.js-tab-nav', function(e){
+                e.preventDefault();
+                var dThisTabNav = $(this);
+                var target = dThisTabNav.find('a').attr('href');
+                target = target.split('#');
+                var dThisTabPanel = dThisTabNav.parents('.js-tabs').find('#'+target[1]);
+                dTabNav.removeClass('selected');
+                dThisTabNav.addClass('selected');
+                dTabPanel.hide();
+                dThisTabPanel.show();
+            });
+			
+			//init
+			dTabNav.eq(config.activeTabNumber).click();
+        }
+		// initialize every element
+		this.each(function() {
+			init($(this));
+		});
+		return this;
+	};
+	// start
+	$(function() {
+		$('.js-tabs').tabsVertical();
+	});
+})(jQuery);
